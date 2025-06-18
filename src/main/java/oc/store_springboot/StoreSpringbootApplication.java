b125f444b1c150d43323d9d575216f89f227bd03
corona94 <octa9484@gmail.com>
2025-06-18 12:38:59 -0600

    Primer cambio

---
+++ src/main/java/oc/store_springboot/StoreSpringbootApplication.java
@@ -0,0 +1,13 @@
+package oc.store_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreSpringbootApplication.class, args);
+	}
+
+}
